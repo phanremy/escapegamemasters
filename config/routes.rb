f8ebb13c05016af49587f45220762654e134d9b2
Remy Phan <phan.remy@gmail.com>
2020-04-07 19:18:33 +0200

    first seed The Game and index and shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+  root to: 'pages#dashboard'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :companies
+
+  resources :users do
+    resources :friends
+  end
+
+  resources :games
+
+  resources :plays do
+    resources :messages
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# the_game = Company.new(name:"The Game", address: "", website:"", description:"in progress")
+
+### My game from the Game ###
+# if Company.find_by(name: "The Game")
+the_game = Company.new(name:"The Game", address: "51 Rue du Cardinal Lemoine, 75005 Paris", website:"https://www.thegame-france.com/", description:"in progress")
+the_game.save
+
+game_casino = Game.new(name:"Braquage de Casino")
+game_casino.company = the_game
+
+game_casino.save
+
+game_francais = Game.new(name:"Braquage A La Francaise")
+game_francais.company = the_game
+
+game_francais.save
+
+
+# Casino
+# won
+# 54:21
+
+
+
+# Braquage a la francaise
+# won
+
+# end
+
+# others
+
+# Pirate
+# Voyage dans le temps
+
+# Rashomon Escape Game
+# Braquage Musee
+# won
+
+
+# Epsilon
+# hopital psychiatrique
+# won
+# 45min
+
