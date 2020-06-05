becd1626c00d86541a014f1d90a2d856a840f9fe
Remy Phan <phan.remy@gmail.com>
2020-06-05 21:34:31 +0200

    stats fixed, error of counting plays without players anymore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @plays = Play.all.select { |play| @user.participations.map(&:play_id).include?(play.id) }
-    @win = @plays.select { |play| play.won && play.done }
-    @lose = @plays.select { |play| play.done unless play.won }
+    @win = @plays.select { |play| play.won }
+    @lose = @plays.select { |play| play.won == false }
+    @one = @win.count + @lose.count
+    @none = @plays.count - @one
     @pending = @plays.select { |play| true unless play.done }
     @user.top ? @top = @user.top : @top = Top.new
   end
