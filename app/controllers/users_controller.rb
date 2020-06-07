4fc3142b46ea232b5a4a65c0f9962aba21ff94e3
Remy Phan <phan.remy@gmail.com>
2020-06-08 00:09:01 +0200

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   autocomplete :users, :full_name, full: true
 
   def index
-    @users = User.all.order(:id)
+    @users = User.all
   end
 
   def show
